06d1f82e3d75344e18368e70463a5f0d864ef739
Luke Mertlich <68026241+SirNugget13@users.noreply.github.com>
2023-05-18 10:18:03 -0700

    yes

--- DireDireDungeonGame/Assets/Scripts/Player Scripts/AnimatedPlayerScript/AnimatedPlayerCombat.cs
+++ DireDireDungeonGame/Assets/Scripts/Player Scripts/AnimatedPlayerScript/AnimatedPlayerCombat.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class AnimatedPlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public CinemachineVirtualCamera vcam;
+    public GameObject Sword;
+    public PlayerController pc;
+
+    public float SwingDelay;
+    private float swingTimer;
+    private bool canSwing = true;
+    private CapsuleCollider2D swordPath;
+
+    private void Start()
     {
-        
+        swordPath = gameObject.GetComponent<CapsuleCollider2D>();
+        swordPath.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pc.state != PlayerController.State.Dead)
+        {
+            if (Input.GetButtonDown("Attack") && !GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().IsPaused && pc.state != PlayerController.State.Dead)
+            {
+                Attack();
+            }
+
+            swingTimer += Time.deltaTime;
+
+            if (swingTimer >= SwingDelay)
+            {
+                canSwing = true;
+            }
+        }
     }
+
+    void Attack()
+    {
+        if (canSwing)
+        {
+            swordPath.enabled = true;
+
+            Sword.transform.SetLocalPositionAndRotation
+                (Vector3.zero, Quaternion.Euler(0, 0, -125));
+            this.Wait(0.2f, () =>
+            {
+                Sword.transform.SetLocalPositionAndRotation
+                    (Vector3.zero, Quaternion.Euler(0, 0, -50));
+                swordPath.enabled = false;
+            });
+
+            canSwing = false;
+            swingTimer = 0;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+
+        }
+    }
+
 }
